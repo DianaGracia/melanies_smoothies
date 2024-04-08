87a791b39ecb722f42050dfce1b1f441971ddc0f
DianaGracia <46504134+DianaGracia@users.noreply.github.com>
2024-04-08 18:24:57 +0200

    Update streamlit_app.py
    added api

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,10 @@ if ingredient_string:
     st.write(my_insert_stmt)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+# st.text(fruityvice_response.json())
+
+fv_df= st.dataframe(data=fruityvice_reponse.json(), use_container_width=True)
+
 time_to_insert = st.button ('Submit order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
